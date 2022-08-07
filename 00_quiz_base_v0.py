453bd273ca96699e9fe9530b6feb2c1767ad0029
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-08-08 11:50:38 +1200

    Skeleton for the Base Code

---
+++ 00_quiz_base_v0.py
@@ -0,0 +1,12 @@
+"""
+Base Component for NZ General Knowledge Quiz
+v0 - skeleton setup
+
+Author - Yuri Dilks
+CC YD 2022
+"""
+
+# Functions go here ---------------------------------
+
+
+# Main Routine goes here ----------------------------
