c91d9b99b13c33f98b766c42ee4c23aa89ad9562
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-08-14 15:04:33 +1200

    Quiz Questions (Version One)
    This is mainly a trial. I have only added three questions. This version is not a multi-choice yet.

---
+++ 04_quiz_questions_v1.py
@@ -0,0 +1,24 @@
+# Functions go here ------------------------------------
+
+
+# Main Routine goes here -------------------------------
+# Question One
+user_answer = input("What is the capital city of New Zealand? ")
+if user_answer == "Wellington":
+    print("Well done, that is correct!")
+else:
+    print("Sorry, that is incorrect. The answer is 'Wellington', not {}.".format(user_answer))
+
+# Question Two
+user_answer = input("How many official languages are there in New Zealand? ")
+if user_answer == "Three":
+    print("Well done, that is correct!")
+else:
+    print("Sorry, that is incorrect. The answer is 'Three', not {}.".format(user_answer))
+
+# Question Three
+user_answer = input("What is New Zealand's official name in Maori ")
+if user_answer == "Aotearoa":
+    print("Well done, that is correct!")
+else:
+    print("Sorry, that is incorrect. The answer is 'Aotearoa', not {}.".format(user_answer))
