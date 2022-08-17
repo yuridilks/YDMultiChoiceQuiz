726fa012fd5cfda422a59811c318624b08402f36
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-08-17 16:44:04 +1200

    Adding Component Two into Working Program

--- 00_quiz_base_v2.py
+++ 00_quiz_base_v2.py
@@ -11,24 +11,29 @@ CC YD 2022
 
 
 # Yes / No Checker function
+# creating a yes / no checker function to check users answers when answer the 'instruction' question
 def yes_no_checker(question):
     valid = False
     while not valid:
         user_response = input(question).lower()
 
+        # if users have played quiz before, program continues
         if user_response == "y" or user_response == "yes":
             user_response = "yes"
             return user_response
 
+        # if users haven't played quiz before, show instructions
         elif user_response == "n" or user_response == "no":
             user_response = "no"
             return user_response
 
+        # printing a statement if user answers invalid input
         else:
             print("Please enter a 'yes' or 'no' answer")
 
 
 # Instruction function
+# creating easy-to-read instructions for users if they haven't played the game before
 def quiz_instruction():
     print()
     print("~~~ How to Play the NZ General Knowledge Quiz ~~~")
@@ -39,8 +44,10 @@ def quiz_instruction():
 
 
 # Main Routine goes here --------------------------
+# asking users if they have played the quiz before
 played_quiz_before = yes_no_checker("Have you played this quiz before? ")
 
+# printing instructions if users answer 'no'
 if played_quiz_before == "no":
     quiz_instruction()
 
