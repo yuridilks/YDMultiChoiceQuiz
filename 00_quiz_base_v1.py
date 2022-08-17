d7efd10e7977428cf6db2697dd42b35534fe9f6d
yuridilks <107890787+yuridilks@users.noreply.github.com>
2022-08-17 16:40:14 +1200

    Adding Component One into Working Program

--- 00_quiz_base_v1.py
+++ 00_quiz_base_v1.py
@@ -1,7 +1,8 @@
 """
 Base Component for NZ General Knowledge Quiz
 v0 - skeleton setup
-v1 - added component one
+v1 - adding component one
+v2 - adding component two
 
 Author - Yuri Dilks
 CC YD 2022
@@ -9,24 +10,29 @@ CC YD 2022
 
 
 # Functions go here ---------------------------------
+# creating a yes / no checker function to check users answers when answer the 'instruction' question
 def yes_no_checker(question):
     valid = False
     while not valid:
         user_response = input(question).lower()
 
+        # if users have played quiz before, program continues
         if user_response == "y" or user_response == "yes":
             user_response = "yes"
             return user_response
 
+        # if users haven't played quiz before, show instructions
         elif user_response == "n" or user_response == "no":
             user_response = "no"
             return user_response
 
+        # printing a statement if user answers invalid input
         else:
             print("Please enter a 'yes' or 'no' answer")
 
 
 # Main Routine goes here ----------------------------
+# asking users if they have played the quiz before
 quiz_instructions = yes_no_checker("Have you played this quiz before? ")
 print("You answered {} to playing this quiz before".format(quiz_instructions))
 start_quiz = yes_no_checker("Are you ready to start the quiz? ")
